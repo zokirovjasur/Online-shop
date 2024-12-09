476bae85f0ca7506363d64bd6c36d077280b3b00
zokirovjasur <jasurbek7682@gmail.com>
2024-12-09 19:08:22 +0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite/plugin";
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
       container: {
         center: true,
-      }
+      },
     },
   },
-  plugins: [require('flowbite/plugin')],
-}
+  plugins: [flowbite],
+};
